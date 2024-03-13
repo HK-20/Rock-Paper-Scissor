6aa7260fae6b83ef1ca2517b34fd3f6fb321eab7
HK-20 <hari20k32@gmail.com>
2024-03-13 18:18:48 +0530

    Edit Index.html and style.css and code.js

---
+++ code.js
@@ -0,0 +1,140 @@
+let playerScore = 0;
+let computerScore  = 0;
+let isGameOver = false;
+
+function checkGameOver(computerScore, playerScore){
+    if(playerScore ===5){
+        isGameOver= true
+        return whenGameEnds(1);}
+        else if(computerScore === 5)
+        {
+            isGameOver = true;
+            
+            return whenGameEnds(0);
+     }
+    }
+    
+
+const displayChoices = (computerSelection) =>
+{
+    if(computerSelection === 0){
+        computerChoice.textContent = "CPU CHOSE: PAPER"
+        compDiv.appendChild(computerChoice);
+        
+    }else if (computerSelection === 1){
+        computerChoice.textContent = "CPU CHOSE: SCISSORS"
+        compDiv.appendChild(computerChoice);
+    }
+    else if(computerSelection === 2){
+        computerChoice.textContent = "CPU CHOSE: ROCK"
+        compDiv.appendChild(computerChoice);
+    }
+    
+}
+
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const rock = document.querySelector('#rock');
+const results = document.querySelector('#results');
+const winLose = document.createElement('p');
+const playerS = document.createElement('p');
+const computerS = document.createElement('p');
+const winmes = document.querySelector('#winmes');
+const computerChoice = document.createElement('p');
+const compDiv = document.querySelector('#compChoice');
+const winMessage = document.createElement('p');
+winMessage.style.fontSize = '100px';
+const scores = document.querySelector('#scores');
+
+const whenGameEnds = (value) =>
+{
+    
+    value === 1 ? winMessage.textContent = 'PLAYER WINS' : winMessage.textContent = 'CPU WINS'
+    winmes.appendChild(winMessage);
+    scissors.disabled= true;
+    paper.disabled = true;
+    rock.disabled = true;
+}
+
+paper.addEventListener('click', () =>{
+    playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+    displayChoices(computerSelection);
+    winLose.textContent = playRound(playerSelection, computerSelection)
+    checkGameOver(computerScore, playerScore);
+    
+})
+
+scissors.addEventListener('click', () =>{
+    playerSelection = "scissors";
+    const computerSelection = getComputerChoice();
+    displayChoices(computerSelection);
+    winLose.textContent = playRound(playerSelection, computerSelection)
+    checkGameOver(computerScore, playerScore);
+    
+})
+
+
+rock.addEventListener('click', () =>{
+    playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    displayChoices(computerSelection);
+    winLose.textContent = playRound(playerSelection, computerSelection);
+    checkGameOver(computerScore, playerScore);
+})
+scores.appendChild(playerS);
+scores.appendChild(computerS);
+results.appendChild(winLose);
+
+function getComputerChoice() { 
+    let x = Math.floor(Math.random() *3)
+    return x;
+}
+function playRound(playerSelection, computerSelection)
+{
+   
+    if(playerSelection === "rock" && computerSelection === 1){ 
+        playerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return 'You win! rock beats scissors!';
+        
+    } 
+    else if(playerSelection === "scissors" && computerSelection === 0){  
+        playerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return'You win! scissors beats paper!';
+        }    
+    else if(playerSelection === "paper" && computerSelection === 2){ 
+        playerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return 'You win! rock beats paper!';
+        }     
+    else if(playerSelection === "scissors" && computerSelection === 2){ 
+        computerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return'You lose! scissors beats rock!';
+        }
+    else if(playerSelection === "paper" && computerSelection === 1){ 
+        computerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return 'You lose! paper beats scissors!'; 
+       }
+    else if(playerSelection === "rock" && computerSelection === 0){ 
+        computerScore++;
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return "You lose! paper beats rock!";
+        }
+    else {
+        computerS.textContent = `CPU SCORE: ${computerScore}`;
+        playerS.textContent = `PLAYER SCORE: ${playerScore}`;
+        return "Draw!";
+    }
+        
+    
+} 
